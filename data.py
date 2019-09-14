8738457ae8e0f0c4bd897020e7a0ef3643fb8a0b
NULL <null.void.null@protonmail.com>
2019-09-14 18:01:02 +0300

    -

--- data.py
+++ data.py
@@ -3,51 +3,13 @@ import cv2
 import numpy as np
 import math
 import os
+from utils import imageResize, centerCrop
 
-
-
-def image_resize(image, width = None, height = None, inter = cv2.INTER_LINEAR):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
-
-    # return the resized image
-    return resized
 
 def preprocess_input(img):
-    if(img.shape[0] > img.shape[1]):
-          frame = image_resize(img,width=256)
-    else:
-          frame = image_resize(img,height=256)  
-
+    frame = imageResize(img,256)
     frame = (frame/255.)*2.0 - 1.0
-    h,w = frame.shape[:2]
-    y = int((h - 224)/2)
-    x = int((w-224)/2)
-    frame = frame[y:(224+y), x:(224+x)]  
+    frame = centerCrop(frame,224)  
     return frame
 
 def readVideoClip(video_fragment, maxClipDuration):
--- utils.py
+++ utils.py
@@ -1,6 +1,33 @@
 import numpy as np
 import json
+import cv2
 
+
+
+def centerCrop(image,dim = 224):
+    h,w = image.shape[:2]
+    y = int((h - dim)/2)
+    x = int((w-dim)/2)
+
+    return image[y:(dim+y), x:(dim+x)]  
+
+
+def imageResize(image, minimumDimension, inter = cv2.INTER_LINEAR):
+
+    dim = None
+    (h, w) = image.shape[:2]
+
+    
+    if(h > w):
+        r = minimumDimension / float(w)
+        dim = (minimumDimension, int(h * r))
+    else:      
+        r = minimumDimension / float(h)
+        dim = (int(w * r), minimumDimension)
+
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    return resized
 
 def writeJsontoFile(fileName, jsonArray):
     with open(fileName, 'w') as f: 
