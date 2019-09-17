c6f86f2e3c7fd09f0ca4a8f095adf08364919639
NULL <null>
2019-09-17 10:15:55 +0300

    -

--- test.py
+++ test.py
@@ -4,7 +4,44 @@ from data import generateDatasetList
 from utils import getPredictions, getTopNindecies ,writeJsontoFile
 
 
-
+def meh(predictions, annotationList, classes, topNpredictions = 10):
+    output= []
+    currentVideo = {'video': annotationList[0]['video'], 'predictions':np.zeros(len(predictions[0]))}
+    c = 0
+    i = 0
+    while(True):
+        if(annotationList[i]['start_frame'] == 0 ):
+            currentVideo['predictions'] += predictions[i]
+            i+=1
+            c+=1
+            while( i < len(predictions) and annotationList[i]['start_frame'] != 0 ):
+                currentVideo['predictions'] += predictions[i]
+                i+=1
+                c+=1
+                
+        currentVideo['predictions'] /= c
+        topPredictions = getTopNindecies(currentVideo['predictions'], topNpredictions)
+        currentPredictions = []
+        for index in topPredictions:
+            label = classes[index]
+            score = currentVideo['predictions'][index].astype('float')
+            currentPredictions.append({'label':label, 'score':score})
+        vidPath = currentVideo['video'].split('/')
+        video = vidPath[len(vidPath)-1][:-18]   
+        output.append({'video': video ,
+                          'label': classes[topPredictions[0]],
+                           'predictions': currentPredictions
+                           })
+            
+            
+        c = 0
+        if  i == len(predictions):
+            break
+        currentVideo = {'video': annotationList[i]['video'], 'predictions':np.zeros(len(predictions[0]))}  
+          
+    return output        
+            
+                
 
 def generateFormatedOutput(predictions, annotationList, classes, topNpredictions = 10):
     output= []
@@ -46,5 +83,5 @@ def test (model, testDirectory, classList, INPUT_FRAMES = 64, batchSize = 10):
     out_logits = out_logits[:len(annotationList)]
     predictions = getPredictions(out_logits)
 
-    output = generateFormatedOutput(predictions,annotationList,classList)
+    output = meh(predictions,annotationList,classList)
     writeJsontoFile('results.json',output)
